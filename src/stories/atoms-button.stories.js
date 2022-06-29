2e0b76e21694025f13220785e38ee8549b4ed69d
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-06-29 15:30:29 +0900

    Update atoms-button.stories.js

--- src/stories/atoms-button.stories.js
+++ src/stories/atoms-button.stories.js
@@ -1,1 +1,6 @@
+import Item_Example from "../components/atoms/example/button.html";
+import "../components/atoms/example/button.scss";
 
+export default { title: "Atoms/Title"};
+
+export const Example = () => Item_Example;
