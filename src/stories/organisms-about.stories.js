4d2488c1a90daed56a274bae9528b7251e1b1f89
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-08-24 16:44:35 +0900

    Delete organisms-about.stories.js

--- src/stories/organisms-about.stories.js
+++
@@ -1,6 +0,0 @@
-import Organisms_About from "../components/organisms/about/about.html";
-import "../components/organisms/about/about.scss";
-
-export default { title: "Organisms/About" };
-
-export const About = () => Organisms_About;
