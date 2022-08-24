37b642629143ba3b7d15a600c48d78fe9a47df9a
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-08-24 16:19:34 +0900

    Update organisms-about.stories.js

--- src/stories/organisms-about.stories.js
+++ src/stories/organisms-about.stories.js
@@ -1,33 +1,6 @@
-@use "../../variables" as *;
-.about {
-  margin-bottom: 40px;
-  background-color: $white;
-  .about-img-wrapper {
-    .about-img {
-      img {
-        width: 100%;
-      }
-    }
-  }
-  .message {
-    text-align: center;
-    padding: 32px 24px;
-    font-size: $fontsize-s;
-    background-color: $white_smoke;
-    h2 {
-      font-size: $fontsize-l;
-      color: $orange_bistre;
-    }
-  }
-}
-@media (min-width: $breakpoint-lg) {
-  .about {
-    padding: 0 16px 100px;
-    .message {
-      padding: 52px 27px;
-      h2 {
-        font-size: $fontsize-xl;
-      }
-    }
-  }
-}
+import Organisms_About from "../components/organisms/about/about.html";
+import "../components/organisms/about/about.scss";
+
+export default { title: "Organisms/About" };
+
+export const About = () => Organisms_About;
