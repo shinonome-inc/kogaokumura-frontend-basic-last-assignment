9c072df40395d32cb7e7374db04d2b642d210f06
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-08-31 21:50:55 +0900

    Create organisms-plans.stories.js

---
+++ src/stories/organisms-plans.stories.js
@@ -0,0 +1,6 @@
+import Organisms_Plans from "../components/organisms/plans/plans.html";
+import "../components/organisms/plans/plans.scss";
+
+export default { title: "Organisms/Plans" };
+
+export const Plans = () => Organisms_Plans;
