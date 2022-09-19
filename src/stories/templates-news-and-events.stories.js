e262004301210b9831c0632e79fe0d8c929fd092
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-09-19 23:10:18 +0900

    Create templates-news-and-events.stories.js

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,6 @@
+import Templates_NewsAndEvents from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+
+export default { title: "Templates/NewsAndEvents" };
+
+export const NewsAndEvents = () => Templates_NewsAndEvents;
